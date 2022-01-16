946590b9738da6e68ec83095c287b11ba9cad9cc
Damjan Savkovic <damjans92@gmail.com>
2022-01-16 16:11:22 +0100

    Github Actions for user and repos, Axios, User search, Alert component

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -3,44 +3,21 @@ import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext();
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
-
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
+    user: {},
+    repos: [],
     loading: false,
   };
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  const fetchUsers = async () => {
-    setLoading();
-    const response = await fetch(`${GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
-
-    const data = await response.json();
-
-    dispatch({
-      type: "GET_USERS",
-      payload: data,
-    });
-  };
-
-  // set loading
-  const setLoading = () => {
-    dispatch({ type: "SET_LOADING" });
-  };
-
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        loading: state.loading,
-        fetchUsers,
+        ...state,
+        dispatch,
       }}
     >
       {children}
