4f90834c4eb999dd69c8f58a52e4013698d488a9
Damjan Savkovic <damjans92@gmail.com>
2022-03-18 20:20:45 +0100

    changed spinner and about page

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -3,7 +3,8 @@ import spinner from "../../assets/spinner.gif";
 function Spinner() {
   return (
     <div className="w-100 mt-20">
-      <img src={spinner} alt="Loading..." className="text-center mx-auto" />
+      {/* <img src={spinner} alt="Loading..." className="text-center mx-auto" /> */}
+      <div className="loader text-center mx-auto"></div>
     </div>
   );
 }
