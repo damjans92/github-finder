9db6649558ebe085e66b6317a27e544f9dfc2b41
Damjan Savkovic <damjans92@gmail.com>
2022-03-19 15:13:07 +0100

    removed import spinner gif

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -1,9 +1,6 @@
-import spinner from "../../assets/spinner.gif";
-
 function Spinner() {
   return (
     <div className="w-100 mt-20">
-      {/* <img src={spinner} alt="Loading..." className="text-center mx-auto" /> */}
       <div className="loader text-center mx-auto"></div>
     </div>
   );
